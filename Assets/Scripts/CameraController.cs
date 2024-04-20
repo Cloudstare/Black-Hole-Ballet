a26996d5fbdf24d85457da43202bff50b7853117
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2024-04-20 13:02:25 +0200

    feat: camera lerp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float xMin, xMax, yMin, yMax;
+    public float yOffset;
+    public float xOffset;
+    public float lerpAmount;
     private GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -12,11 +14,13 @@ public class CameraController : MonoBehaviour
        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
-        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
-        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
-    }
+{
+    float x = player.transform.position.x;
+    float y = player.transform.position.y + yOffset;
+    float direction = player.transform.localScale.x;
+    Vector3 targetPosition = new Vector3(x, y, gameObject.transform.position.z);
+    Vector3 offset = new Vector3(xOffset * Mathf.Sign(direction), 0, 0);
+    gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition + offset, lerpAmount);
+}
 }
