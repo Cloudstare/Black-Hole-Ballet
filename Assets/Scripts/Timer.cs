729d36221fdbb81a112684005f07136ff39ead0f
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2024-04-21 00:01:38 +0200

    feat: timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Dodaj tę linię
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 0.0f;
+    public TMP_Text timerText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.text = "Time: " + timer.ToString("F1"); // F2 oznacza formatowanie do dwóch miejsc po przecinku
+    }
+}
