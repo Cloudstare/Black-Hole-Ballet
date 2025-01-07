ee1fe9290ba6609c0bd5b8517140a92dba8dcc0f
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2025-01-07 01:21:01 +0100

    del

--- Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
+++ Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
@@ -3,13 +3,6 @@ using UnityEngine;
 
 namespace TarodevController
 {
-    /// <summary>
-    /// Hey!
-    /// Tarodev here. I built this controller as there was a severe lack of quality & free 2D controllers out there.
-    /// I have a premium version on Patreon, which has every feature you'd expect from a polished controller. Link: https://www.patreon.com/tarodev
-    /// You can play and compete for best times here: https://tarodev.itch.io/extended-ultimate-2d-controller
-    /// If you hve any questions or would like to brag about your score, come to discord: https://discord.gg/tarodev
-    /// </summary>
     [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class PlayerController : MonoBehaviour, IPlayerController
     {
