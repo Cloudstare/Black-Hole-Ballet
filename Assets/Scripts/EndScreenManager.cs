62717ed4275ffefc5145836d228822351b9b995b
Xsesha <luksen2100@gmail.com>
2025-01-12 20:22:55 +0100

    leaderboard working

--- Assets/Scripts/EndScreenManager.cs
+++ Assets/Scripts/EndScreenManager.cs
@@ -16,43 +16,18 @@ public class EndScreenManager : MonoBehaviour
         gameObject.SetActive(false); // Ukryj ekran końcowy na początku
     }
 
+
     public void ShowEndScreen(float gameTime)
     {
         gameObject.SetActive(true);
 
-        // Get game times
-        List<float> gameTimes = GameManager.Instance.GetGameTimes();
+        
 
-        // Create a list of pairs (time, player name)
-        List<(float time, string name)> scores = new List<(float, string)>();
-        for (int i = 0; i < gameTimes.Count; i++)
-        {
-            scores.Add((gameTimes[i], "Player"));
-        }
-
-        // Sort and take top 7 scores
-        scores = scores.OrderBy(score => score.time).Take(7).ToList();
-
-        // Prepare the final text
-        string timesText = "Congratulations!\nTop Scores:\n\n";
-        int place = 1;
-        foreach (var score in scores)
-        {
-            int minutes = Mathf.FloorToInt(score.time / 60F);
-            int seconds = Mathf.FloorToInt(score.time % 60F);
-            timesText += string.Format("{0}. {1} - {2:00}:{3:00}\n", place, score.name, minutes, seconds);
-            place++;
-        }
-
-        // Display the results on the end screen
-        endScreenText.text = timesText;
-
-        // Disable player movement and make player disappear
+        // Disable player movement
         GameObject player = GameObject.FindWithTag("Player");
         if (player != null)
         {
             player.GetComponent<PlayerController>().enabled = false;
-            player.SetActive(false); // Make player disappear
         }
     }
 
--- Assets/Scripts/LeaderboardDisplay.cs
+++ Assets/Scripts/LeaderboardDisplay.cs
@@ -1,34 +1,35 @@
-using TMPro;  // Required for TextMeshPro
 using UnityEngine;
+using TMPro;
 using System.Collections.Generic;
 using System.Linq;
 
+
 public class LeaderboardDisplay : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI leaderboardText;  // Drag your TextMeshPro object here
+    [SerializeField] private TextMeshProUGUI leaderboardText;
 
     private void Start()
     {
         DisplayLeaderboard();
     }
 
-    private void DisplayLeaderboard()
+    public void DisplayLeaderboard()
     {
-        // Get game times from GameManager
         List<float> gameTimes = GameManager.Instance.GetGameTimes();
 
-        // Create list of scores (time, player name)
-        List<(float time, string name)> scores = new List<(float, string)>();
-        for (int i = 0; i < gameTimes.Count; i++)
+        if (gameTimes == null || gameTimes.Count == 0)
         {
-            scores.Add((gameTimes[i], "Player"));
+            leaderboardText.text = "No scores yet! Play to set your best times.";
+            return;
         }
 
-        // Sort scores and take the top 7
-        scores = scores.OrderBy(score => score.time).Take(7).ToList();
+        List<(float time, string name)> scores = gameTimes
+            .Select(time => (time, "Player"))
+            .OrderBy(score => score.time)
+            .Take(7)
+            .ToList();
 
-        // Prepare leaderboard text
-        string timesText = "Congratulations!\nTop Scores:\n\n";
+        string timesText = "";
         int place = 1;
         foreach (var score in scores)
         {
@@ -38,7 +39,15 @@ public class LeaderboardDisplay : MonoBehaviour
             place++;
         }
 
-        // Display on screen
         leaderboardText.text = timesText;
+    }
+    public void ClearScores()
+    {
+        PlayerPrefs.DeleteKey("GameTimesCount");
+        for (int i = 0; i < 100; i++) // Assuming a maximum of 100 scores
+        {
+            PlayerPrefs.DeleteKey("GameTime" + i);
+        }
+        DisplayLeaderboard();
     }
 }
